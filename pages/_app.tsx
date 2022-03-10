44b22135c6e60bb3fcca2ab5883f9f32f7f2d6fc
김태영 <kimtaeyoung@0Taes-Mac.local>
2022-03-10 17:31:36 +0900

    seo 이미지 오타 수정

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -16,7 +16,7 @@ function MyApp({ Component, pageProps }: AppProps) {
           description: '부산 초등학생 코딩 교육',
           images: [
             {
-              url: 'https://codingkids/vercel.app/coding_kids_background.jpg',
+              url: 'https://codingkids.vercel.app/coding_kids_background.jpg',
               width: 800,
               height: 600,
               alt: '코딩키드',
