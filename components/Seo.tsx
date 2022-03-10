dd8f6920aac80b13c1d7f639147772d2510b6f62
김태영 <kimtaeyoung@0Taes-Mac.local>
2022-03-10 17:27:49 +0900

    seo 작업

---
+++ components/Seo.tsx
@@ -0,0 +1,17 @@
+import Head from 'next/head';
+
+interface IProps {
+  title: string;
+}
+
+export default function Seo({ title }: IProps) {
+  return (
+    <Head>
+      {title ? (
+        <title>{title} | 코딩키드</title>
+      ) : (
+        <title>코딩키드 - 부산 초등학생 코딩 수업</title>
+      )}
+    </Head>
+  );
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,31 @@
 import '@styles/globals.css';
 import type { AppProps } from 'next/app';
 import Layout from '@components/Layout';
+import { NextSeo } from 'next-seo';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Layout>
+      <NextSeo
+        title='코딩키드'
+        description='부산 초등학생 코딩 교육'
+        canonical='https://codingkids.vercel.app'
+        openGraph={{
+          url: 'https://codingkids.vercel.app',
+          title: '코딩키드',
+          description: '부산 초등학생 코딩 교육',
+          images: [
+            {
+              url: 'https://codingkids/vercel.app/coding_kids_background.jpg',
+              width: 800,
+              height: 600,
+              alt: '코딩키드',
+              type: 'image/jpeg',
+            },
+          ],
+          site_name: '코딩키드',
+        }}
+      />
       <Component {...pageProps} />
     </Layout>
   );
