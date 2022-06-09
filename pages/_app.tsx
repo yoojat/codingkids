48501448ba0e02814779206cc5cc131221a882b0
김태영 <kimtaeyoung@0Taes-Mac.local>
2022-06-09 15:34:52 +0900

    description 수정

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,13 +8,13 @@ function MyApp({ Component, pageProps }: AppProps) {
     <Layout>
       <NextSeo
         title='부산코딩스쿨 - 부산코딩학원'
-        description='초중고 대상의 부산코딩학원입니다. 현직 프로그래머들이 빠르게 변화하는 트렌드를 전하며, 미래를 선도하는 인재를 양성합니다.'
+        description='초중고 대상의 부산코딩교육을 진행합니다. 현직 프로그래머들이 빠르게 변화하는 트렌드를 전하며, 미래를 선도하는 인재를 양성합니다. 사직동, 서면, 센텀, 화명동에서 교육을 진행하고 있습니다. '
         canonical='https://busancodingschool.vercel.app'
         openGraph={{
           url: 'https://busancodingschool.vercel.app',
           title: '부산코딩스쿨 - 부산코딩학원',
           description:
-            '초중고 대상의 부산코딩학원입니다. 현직 프로그래머들이 빠르게 변화하는 트렌드를 전하며, 미래를 선도하는 인재를 양성합니다.',
+            '초중고 대상의 부산코딩교육을 진행합니다. 현직 프로그래머들이 빠르게 변화하는 트렌드를 전하며, 미래를 선도하는 인재를 양성합니다. 사직동, 서면, 센텀, 화명동에서 교육을 진행하고 있습니다. ',
           images: [
             {
               url: 'https://busancodingschool.vercel.app/coding_kids_background.jpg',
